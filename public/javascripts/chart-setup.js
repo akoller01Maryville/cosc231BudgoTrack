2f0fad5e3bff15654b1c7ccce2cab87b4306678c
gnawtough <avkoller01@hotmail.com>
2024-03-31 21:27:23 -0500

    add html and chart

---
+++ public/javascripts/chart-setup.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx1 = document.getElementById('chart1').getContext('2d');
+    var myChart1 = new Chart(ctx1, {
+        type: 'bar', // bar graph example
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // Second chart
+    var ctx2 = document.getElementById('chart2').getContext('2d');
+    var myChart2 = new Chart(ctx2, {
+        type: 'pie', // pie chart example
+        data: {
+            labels: ['Red', 'Blue', 'Yellow'],
+            datasets: [{
+                label: 'Dataset 1',
+                data: [300, 50, 100],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                hoverOffset: 4
+            }]
+        },
+    });
+});
