af803d80ce0de26ac022fef4e4c558d6dd1c53c6
gnawtough <avkoller01@hotmail.com>
2024-04-01 21:32:14 -0500

    clean up models and db script

--- models/index.js
+++ models/index.js
@@ -1,13 +1,19 @@
 const Sequelize = require('sequelize');
 
+// create sequelize instance with database info
 const sequelize = new Sequelize('testDB', 'gnawtough', "#RoED#7R$9pK%7KFVXvBTMcPuki7a^ae", {
     host: 'localhost',
     dialect: 'mysql',
 });
 
-const User = require('./user');
+//import models
+const User = require('./user')(sequelize, Sequelize.DataTypes);
+
+sequelize.sync()
+    .then(() => console.log('Models synchronized successfully.'))
+    .catch(error => console.error('Error synchronizing models:', error));
 
 module.exports = {
-    User,
     sequelize,
+    User
 };
--- models/user.js
+++ models/user.js
@@ -1,21 +1,15 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('testDB', 'gnawtough', '#RoED#7R$9pK%7KFVXvBTMcPuki7a^ae', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
 
-const User = sequelize.define('user', {
-    username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-});
-
-sequelize.sync();
-
-module.exports = User;
+    return User;
+};
