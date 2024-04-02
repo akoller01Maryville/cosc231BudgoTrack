cfbf86cbbe3354f1d8504dbf6c07a1126e3a9043
gnawtough <avkoller01@hotmail.com>
2024-04-02 17:46:46 -0500

    add setupDatabase initialization script

---
+++ scripts/setupDatabase.js
@@ -0,0 +1,18 @@
+// run to initialize database
+const { sequelize } = require('../models');
+
+
+async function initializeDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to the database has been established successfully.');
+
+        await sequelize.sync(/* { force: true } // Use with caution! */);
+        console.log('Database schema has been synchronized successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database or synchronize schema:', error);
+    }
+}
+
+//change parameter to True if want to reinitialize clean database.
+initializeDatabase();
