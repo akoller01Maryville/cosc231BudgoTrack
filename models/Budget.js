b6b695a6622fa164088c80b6ca9b0dc1f531b78c
piwIStoner <38994816+piwIStoner@users.noreply.github.com>
2024-04-08 08:52:51 -0700

    Update Budget.js

--- models/Budget.js
+++ models/Budget.js
@@ -1,1 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const budget = sequelize.define('user', {
+        StartDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        EndDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        TotalAmount: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+        },
+    });
 
+    return User;
