2f481340d4ef2d0beb5a9682c0e37f49158c2da8
piwIStoner <38994816+piwIStoner@users.noreply.github.com>
2024-04-08 08:41:44 -0700

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,1 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('user', {
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        Description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
 
+    return User;
