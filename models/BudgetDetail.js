08d9af9d5ce49ee81e29c525d083fafa2cfd12cb
piwIStoner <38994816+piwIStoner@users.noreply.github.com>
2024-04-08 09:06:14 -0700

    Update BudgetDetail.js

--- models/BudgetDetail.js
+++ models/BudgetDetail.js
@@ -1,1 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+    const budgetdetail = sequelize.define('user', {
+        AllocatedAmount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
+    });
 
+    return budgetdetail;
+};
