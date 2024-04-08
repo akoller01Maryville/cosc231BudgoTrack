cc2a6cc5a0764f3b348444d816cc488c9dcbe819
piwIStoner <38994816+piwIStoner@users.noreply.github.com>
2024-04-08 08:01:25 -0700

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,1 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    const Transaction = sequelize.define('receipt', {
+        PurchaseDate: {
+            type: DataTypes.DATE,
+            unique: false,
+            allowNull: false,
+        },
 
+        TotalAmount: {
+            type: DataTypes.Decimal(10,2),
+            unique: false,
+            allowNull: false,
+        },
+
+        StoreName: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false,
+        },
+        // TODO: add other columns for this table
+    });
+
+    return Receipt;
+};
