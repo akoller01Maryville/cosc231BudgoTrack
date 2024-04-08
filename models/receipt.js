7c7975e03519d73bae92387d4389e61235508cf4
piwIStoner <38994816+piwIStoner@users.noreply.github.com>
2024-04-07 21:37:34 -0700

    Update receipt.js

--- models/receipt.js
+++ models/receipt.js
@@ -6,6 +6,18 @@ module.exports = (sequelize, DataTypes) => {
             unique: false,
             allowNull: false,
         },
+
+        TotalAmount: {
+            type: DataTypes.Decimal(10,2),
+            unique: false,
+            allowNull: false,
+        },
+
+        StoreName: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false,
+        },
         // TODO: add other columns for this table
     });
 
