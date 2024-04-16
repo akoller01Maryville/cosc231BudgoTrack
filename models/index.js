9bc8b0b096bde8c9787c60b73d3d1235d2a35b75
gnawtough <avkoller01@hotmail.com>
2024-04-16 10:13:09 -0500

    import models to index.js

--- models/index.js
+++ models/index.js
@@ -9,6 +9,16 @@ const sequelize = new Sequelize('testDB', 'gnawtough', "#RoED#7R$9pK%7KFVXvBTMcP
 
 //import models TODO: add all other models to this and add to exports
 const User = require('./user')(sequelize, Sequelize.DataTypes);
+const Analysis = require('./analysis')(sequelize, Sequelize.DataTypes);
+const Budget = require('./budget')(sequelize, Sequelize.DataTypes);
+const BudgetDetails = require('./budgetDetails')(sequelize, Sequelize.DataTypes);
+const Category = require('./category')(sequelize, Sequelize.DataTypes);
+const Receipt = require('./receipt')(sequelize, Sequelize.DataTypes);
+const Transaction = require('./transaction')(sequelize, Sequelize.DataTypes);
+const UserPreferences = require('./userPreferences')(sequelize, Sequelize.DataTypes);
+const UserProfile = require('./userProfile')(sequelize, Sequelize.DataTypes);
+const Visualization = require('./visualization')(sequelize, Sequelize.DataTypes);
+
 
 // establish relationships TODO: add relationships for each tables based on chart
 // example 1:
